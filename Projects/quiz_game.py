a7e6df70021355020c1da4b97b7bacae0550fe84
Ro-dog-diggs32 <roanmcnae@seattleacademy.org>
2025-03-28 21:35:27 +0000

    hi

--- Projects/quiz_game.py
+++ Projects/quiz_game.py
@@ -3,20 +3,27 @@ bonus_score = 0
 overall_score = 0
 
 answer = input ("what does the aileron do on a plane, A) Roll the plane left to right, or B) pitch it up and down")
-if answer == "A" : 
+if answer == "A": 
     print("correct")
     score += 1
+elif answer == "B": 
+    print("incorrect")
+
 
 answer = input ("what is a air foil, A) a piece of foil filled with air,or B) a certain shape the generates lift when air runs over it")
 if answer == "B" : 
     print("correct")
     score += 1
+elif answer == "A": 
+    print("incorrect")
     
     
 answer = input ("what is the body of a plane called, A) a plane body, or B) a fusilage")
 if answer == "B": 
     print("correct")
     score += 1
+elif answer == "A": 
+    print("incorrect")
 
 
 
@@ -24,6 +31,8 @@ answer = input ("what are flaps on a plane, A) they fold down generating lift an
 if answer == "A":
     print ("correct") 
     bonus_score += 1
+elif answer == "B": 
+    print("incorrect")
 
 
 
@@ -31,18 +40,21 @@ answer = input ("how do the control surfaces effect the plane, A) do they allow 
 if answer == "B":
     print ("correct") 
     bonus_score += 1
+elif answer == "A": 
+    print("incorrect")
 
 
  # end of quiz:
 if score > 2:
     print ("you know your planes")
 
-if bonus_score > 1 and score > 2:
-    overall_score = score + bonus_score
+if bonus_score > 1 and score > 2:  
     print ("im impressed on you knowledge")
 
-if score < 3: 
+if overall_score < 3: 
     print ("you suck at planes")
 
+
+overall_score = score + bonus_score
 print ("you overall score is",overall_score,"out of 5")
 
