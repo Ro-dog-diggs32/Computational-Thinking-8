0860a91fe10b77e2b237b49cfa73ef388b2220ce
Ro-dog-diggs32 <roanmcnae@seattleacademy.org>
2025-04-09 21:57:07 +0000

    hi

--- Projects/shape_thing.py
+++ Projects/shape_thing.py
@@ -5,7 +5,7 @@ t = turtle.Turtle()
 
 
 
-
+#this allows it transition without drawling 
 t.penup()
 t.pensize(3)
 t.speed(0)
@@ -13,6 +13,364 @@ t.goto(0,0)
 t.color("black")
 t.pendown()
 
+#this creates a cool shape that spirals 
+for i in range(22):
+    for j in range(20 - 1*i):
+        t.forward(5)
+        t.left(90)
+        t.forward(10)
+        t.right(90)
+        t.forward(5)
+        t.right(90)
+        t.forward(10)
+        t.left(90)
+        t.forward(5)
+        t.left(90)
+        t.forward(15)
+        t.left(90)
+        t.forward(5)
+        t.right(90)
+        t.forward(5)
+        t.right(90)
+        t.forward(15)
+        t.right(90)
+        t.forward(5)
+        t.right(90)
+        t.forward(5)
+        t.left(90)
+        t.forward(15)
+        t.left(90)
+    
+
+    t.forward(10)
+    t.left(90)
+    t.forward(10)
+
+
+t.penup()
+t.left(210)
+t.goto(0,0)
+t.color("cyan")
+t.pendown()
+
+
+for i in range(22):
+    for j in range(20 - 1*i):
+        t.forward(5)
+        t.left(90)
+        t.forward(10)
+        t.right(90)
+        t.forward(5)
+        t.right(90)
+        t.forward(10)
+        t.left(90)
+        t.forward(5)
+        t.left(90)
+        t.forward(15)
+        t.left(90)
+        t.forward(5)
+        t.right(90)
+        t.forward(5)
+        t.right(90)
+        t.forward(15)
+        t.right(90)
+        t.forward(5)
+        t.right(90)
+        t.forward(5)
+        t.left(90)
+        t.forward(15)
+        t.left(90)
+    
+
+    t.forward(10)
+    t.left(90)
+    t.forward(10)
+
+t.penup()
+t.left(210)
+t.goto(0,0)
+t.color("green")
+t.pendown()
+
+
+for i in range(22):
+    for j in range(20 - 1*i):
+        t.forward(5)
+        t.left(90)
+        t.forward(10)
+        t.right(90)
+        t.forward(5)
+        t.right(90)
+        t.forward(10)
+        t.left(90)
+        t.forward(5)
+        t.left(90)
+        t.forward(15)
+        t.left(90)
+        t.forward(5)
+        t.right(90)
+        t.forward(5)
+        t.right(90)
+        t.forward(15)
+        t.right(90)
+        t.forward(5)
+        t.right(90)
+        t.forward(5)
+        t.left(90)
+        t.forward(15)
+        t.left(90)
+    
+
+    t.forward(10)
+    t.left(90)
+    t.forward(10)
+
+t.penup()
+t.left(210)
+t.goto(0,0)
+t.color("red")
+t.pendown()
+
+
+for i in range(22):
+    for j in range(20 - 1*i):
+        t.forward(5)
+        t.left(90)
+        t.forward(10)
+        t.right(90)
+        t.forward(5)
+        t.right(90)
+        t.forward(10)
+        t.left(90)
+        t.forward(5)
+        t.left(90)
+        t.forward(15)
+        t.left(90)
+        t.forward(5)
+        t.right(90)
+        t.forward(5)
+        t.right(90)
+        t.forward(15)
+        t.right(90)
+        t.forward(5)
+        t.right(90)
+        t.forward(5)
+        t.left(90)
+        t.forward(15)
+        t.left(90)
+    
+
+    t.forward(10)
+    t.left(90)
+    t.forward(10)
+
+
+t.penup()
+t.left(210)
+t.goto(0,0)
+t.color("blue")
+t.pendown()
+
+
+for i in range(22):
+    for j in range(20 - 1*i):
+        t.forward(5)
+        t.left(90)
+        t.forward(10)
+        t.right(90)
+        t.forward(5)
+        t.right(90)
+        t.forward(10)
+        t.left(90)
+        t.forward(5)
+        t.left(90)
+        t.forward(15)
+        t.left(90)
+        t.forward(5)
+        t.right(90)
+        t.forward(5)
+        t.right(90)
+        t.forward(15)
+        t.right(90)
+        t.forward(5)
+        t.right(90)
+        t.forward(5)
+        t.left(90)
+        t.forward(15)
+        t.left(90)
+    
+
+    t.forward(10)
+    t.left(90)
+    t.forward(10)
+
+
+t.penup()
+t.left(210)
+t.goto(0,0)
+t.color("pink")
+t.pendown()
+
+
+for i in range(22):
+    for j in range(20 - 1*i):
+        t.forward(5)
+        t.left(90)
+        t.forward(10)
+        t.right(90)
+        t.forward(5)
+        t.right(90)
+        t.forward(10)
+        t.left(90)
+        t.forward(5)
+        t.left(90)
+        t.forward(15)
+        t.left(90)
+        t.forward(5)
+        t.right(90)
+        t.forward(5)
+        t.right(90)
+        t.forward(15)
+        t.right(90)
+        t.forward(5)
+        t.right(90)
+        t.forward(5)
+        t.left(90)
+        t.forward(15)
+        t.left(90)
+    
+
+    t.forward(10)
+    t.left(90)
+    t.forward(10)
+
+
+t.penup()
+t.left(210)
+t.goto(0,0)
+t.color("orange")
+t.pendown()
+
+
+for i in range(22):
+    for j in range(20 - 1*i):
+        t.forward(5)
+        t.left(90)
+        t.forward(10)
+        t.right(90)
+        t.forward(5)
+        t.right(90)
+        t.forward(10)
+        t.left(90)
+        t.forward(5)
+        t.left(90)
+        t.forward(15)
+        t.left(90)
+        t.forward(5)
+        t.right(90)
+        t.forward(5)
+        t.right(90)
+        t.forward(15)
+        t.right(90)
+        t.forward(5)
+        t.right(90)
+        t.forward(5)
+        t.left(90)
+        t.forward(15)
+        t.left(90)
+    
+
+    t.forward(10)
+    t.left(90)
+    t.forward(10)
+
+
+t.penup()
+t.left(210)
+t.goto(0,0)
+t.color("yellow")
+t.pendown()
+
+
+for i in range(22):
+    for j in range(20 - 1*i):
+        t.forward(5)
+        t.left(90)
+        t.forward(10)
+        t.right(90)
+        t.forward(5)
+        t.right(90)
+        t.forward(10)
+        t.left(90)
+        t.forward(5)
+        t.left(90)
+        t.forward(15)
+        t.left(90)
+        t.forward(5)
+        t.right(90)
+        t.forward(5)
+        t.right(90)
+        t.forward(15)
+        t.right(90)
+        t.forward(5)
+        t.right(90)
+        t.forward(5)
+        t.left(90)
+        t.forward(15)
+        t.left(90)
+    
+
+    t.forward(10)
+    t.left(90)
+    t.forward(10)
+
+
+t.penup()
+t.left(210)
+t.goto(0,0)
+t.color("brown")
+t.pendown()
+
+
+for i in range(22):
+    for j in range(20 - 1*i):
+        t.forward(5)
+        t.left(90)
+        t.forward(10)
+        t.right(90)
+        t.forward(5)
+        t.right(90)
+        t.forward(10)
+        t.left(90)
+        t.forward(5)
+        t.left(90)
+        t.forward(15)
+        t.left(90)
+        t.forward(5)
+        t.right(90)
+        t.forward(5)
+        t.right(90)
+        t.forward(15)
+        t.right(90)
+        t.forward(5)
+        t.right(90)
+        t.forward(5)
+        t.left(90)
+        t.forward(15)
+        t.left(90)
+    
+
+    t.forward(10)
+    t.left(90)
+    t.forward(10)
+
+
+t.penup()
+t.left(210)
+t.goto(0,0)
+t.color("gold")
+t.pendown()
+
 
 for i in range(22):
     for j in range(20 - 1*i):
@@ -86,49 +444,11 @@ for i in range(22):
     t.left(90)
     t.forward(10)
 
-t.penup()
-t.left(210)
-t.goto(0,0)
-t.color("black")
-t.pendown()
-
-
-for i in range(22):
-    for j in range(20 - 1*i):
-        t.forward(5)
-        t.left(90)
-        t.forward(10)
-        t.right(90)
-        t.forward(5)
-        t.right(90)
-        t.forward(10)
-        t.left(90)
-        t.forward(5)
-        t.left(90)
-        t.forward(15)
-        t.left(90)
-        t.forward(5)
-        t.right(90)
-        t.forward(5)
-        t.right(90)
-        t.forward(15)
-        t.right(90)
-        t.forward(5)
-        t.right(90)
-        t.forward(5)
-        t.left(90)
-        t.forward(15)
-        t.left(90)
-    
-
-    t.forward(10)
-    t.left(90)
-    t.forward(10)
 
 t.penup()
 t.left(210)
 t.goto(0,0)
-t.color("black")
+t.color("cyan")
 t.pendown()
 
 
@@ -168,7 +488,47 @@ for i in range(22):
 t.penup()
 t.left(210)
 t.goto(0,0)
-t.color("black")
+t.color("green")
+t.pendown()
+
+
+for i in range(22):
+    for j in range(20 - 1*i):
+        t.forward(5)
+        t.left(90)
+        t.forward(10)
+        t.right(90)
+        t.forward(5)
+        t.right(90)
+        t.forward(10)
+        t.left(90)
+        t.forward(5)
+        t.left(90)
+        t.forward(15)
+        t.left(90)
+        t.forward(5)
+        t.right(90)
+        t.forward(5)
+        t.right(90)
+        t.forward(15)
+        t.right(90)
+        t.forward(5)
+        t.right(90)
+        t.forward(5)
+        t.left(90)
+        t.forward(15)
+        t.left(90)
+    
+
+    t.forward(10)
+    t.left(90)
+    t.forward(10)
+
+
+t.penup()
+t.left(210)
+t.goto(0,0)
+t.color("red")
 t.pendown()
 
 
@@ -206,5 +566,6 @@ for i in range(22):
 
 
 
+#this ends the program 
 turtle.exitonclick()
 
